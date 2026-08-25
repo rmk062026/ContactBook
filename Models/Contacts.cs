f7f0c3903a6d887fac24a72a6945968adc75aa52
rmk062026 <rmk062026@gmail.com>
2026-08-25 12:30:30 +0200

    Added Contacts.cs and data in Contacs

---
+++ Models/Contacts.cs
@@ -0,0 +1,8 @@
+public class Contacts
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Address { get; set; }
+    public string? Email { get; set; }
+}
