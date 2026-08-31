d945ebbcae07ea145c6b4134326d26a4b2dad227
rmk062026 <martin.raudstein@gmail.com>
2026-08-31 10:51:37 +0200

    Added json saving. Testing if json file is uploaded or not

--- Data/ContactFileRepository.cs
+++ Data/ContactFileRepository.cs
@@ -6,10 +6,14 @@ namespace ContactBook.Data;
 public class ContactFileRepository
 {
     private readonly string filePath = "contacts.json";
+    private readonly JsonSerializerOptions options = new()
+    {
+        WriteIndented = true
+    };
 
     public void SaveContacts(IEnumerable<Contact> contacts)
     {
-        string json = JsonSerializer.Serialize(contacts);
+        string json = JsonSerializer.Serialize(contacts, options);
         File.WriteAllText(filePath, json);
     }
 }
