d6f6827b743553275f75a7f8d9dab8ce5833ee7b
rmk062026 <rmk062026@gmail.com>
2026-08-31 14:29:57 +0200

    Added ReadRequiredString to name

--- Program.cs
+++ Program.cs
@@ -27,14 +27,16 @@ class Program
                 case "1":
                     Console.Clear();
 
-                    Console.WriteLine("Enter name:");
-                    string name = Console.ReadLine() ?? "";
+                    // Console.WriteLine("Enter name:");
+                    // string name = Console.ReadLine() ?? "";
+                    string name = ReadRequiredString("Enter name: ");
 
                     Console.WriteLine("Enter phone number");
                     string phoneNumber = Console.ReadLine() ?? "";
 
                     Console.WriteLine("Enter Address");
                     string address = Console.ReadLine() ?? "";
+                    // string address = ReadRequiredString("Enter address: ");
 
                     Console.WriteLine("Enter email:");
                     string email = Console.ReadLine() ?? "";
